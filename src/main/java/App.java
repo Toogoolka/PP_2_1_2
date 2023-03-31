2cae501700243111d2b1500ae89e53f5d4ac6c4d
Toogoolka <120179327+Toogoolka@users.noreply.github.com>
2023-03-31 20:01:28 +0300

    completed pp 2-1-3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanHelloWorld =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getName());
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getName());
+
+
+        System.out.println("Сравниваю хэллоу: " + (bean == beanHelloWorld) +
+                "\nСравниваю котов: " + (cat1 == cat2));
     }
 }
